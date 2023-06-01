664337d1760a925f6d7cb4c40b91d9b0d00d7d55
2964925185@qq.com <2964925185@qq.com>
2023-06-01 14:03:19 +0800

    first commit

---
+++ src/main/java/com/zxx/musicapp/controller/DefaultController.java
@@ -0,0 +1,14 @@
+package com.zxx.musicapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class DefaultController {
+    @GetMapping
+    public String sayHello() {
+        return "hello world";
+    }
+}
